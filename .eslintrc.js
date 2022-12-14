9043c73008792445fad5078b723b8bf73c98c7ff
wangfeiyu@goldmantis.com <wangfeiyu@goldmantis.com>
2022-12-14 15:14:18 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     },
     globals: {
+        uniCloud: true,
         wx: 'readonly',
         uni: 'readonly',
     },
